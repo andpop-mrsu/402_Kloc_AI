1d1e0a98cb644c34dbcc1486180935a32865c673
KlotsAI <57152844+KlotsAI@users.noreply.github.com>
2021-10-15 16:50:59 +0300

    Update Controller.php

--- Task04/guess_number/src/Controller.php
+++ Task04/guess_number/src/Controller.php
@@ -4,7 +4,7 @@ namespace klotsai\guess_number\Controller;
 
 use function klotsai\guess_number\Model\setting;
 use function klotsai\guess_number\View\MenuGame;
-use function klotsai\guess_number\DataBase\openDatabase;
+use function klotsai\guess_number\DB\openDatabase;
 
 function startGame()
 {
