698800308007ce62a0d484c2988bd70eaa667fb3
artur_klots <artur.ak108@gmail.com>
2021-10-15 16:25:53 +0300

    Solution #4

---
+++ Task04/guess_number/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e696512fb08d229f6b201e1ab63b53f159562fc0',
+        'name' => 'klotsai/guess_number',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'klotsai/guess_number' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e696512fb08d229f6b201e1ab63b53f159562fc0',
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.13',
+            'version' => '0.11.13.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'reference' => 'a2866855ac1abc53005c102e901553ad5772dc04',
+            'dev_requirement' => false,
+        ),
+    ),
+);
