eb2b39663c25fcde10ee2f855157b5c2acfb3429
Andrey Popov <andrvpopov@gmail.com>
2022-01-27 08:02:41 +0300

    Make db-apps branch

--- Task02/guess-number/src/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace klotsai\guess_number\Controller;
-use function klotsai\guess_number\View\showGame;
-
-function startGame(){
-   echo "Game started" .PHP_EOL;
-   showGame();
-}
-?>
--- Task02/guess-number/src/View.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    namespace klotsai\guess_number\View;
-    function showGame() {
-        echo "Game shown".PHP_EOL;
-    }
-?>
